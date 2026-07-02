5a76165972211a0492bd5d728fdada1b79e0cadb
서현규 <omnipede@naver.com>
2026-07-02 13:07:25 +0900

    기타: vLLM AWQ GPU 개수 수정

--- vllm_awq.sh
+++ vllm_awq.sh
@@ -3,7 +3,7 @@
 #SBATCH --comment="LLM server"
 #SBATCH --nodes=1
 #SBATCH --nodelist=cubox01,cubox02,cubox03,cubox04,cubox05,cubox06,cubox07,cubox08,cubox09,cubox10,cubox11,cubox12,cubox13,cubox14,cubox15,cubox16
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:2
 #SBATCH --cpus-per-task=28
 #SBATCH --mem-per-cpu=8G
 #SBATCH --output=logs/gem4_awq_31b_%j.log
@@ -31,7 +31,7 @@ python -m vllm.entrypoints.openai.api_server \
     --port 8003 \
     --model $MODEL \
     --served-model-name $SERVED_MODEL_NAME \
-    --tensor-parallel-size 1 \
+    --tensor-parallel-size 2 \
     --max-num-seqs 128 \
     --reasoning-parser gemma4 --enable-auto-tool-choice --tool-call-parser gemma4 \
     --disable-custom-all-reduce \
